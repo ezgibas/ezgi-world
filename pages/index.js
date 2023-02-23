c7fb4d7d17affbd51af51c81f16e71ed37a4040d
Ezgi Bas <52749796+ezgibas@users.noreply.github.com>
2023-02-22 20:46:17 -0500

    update layout and readme

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,13 @@ export default function Home() {
       </Head>
       <header>
         <div className={styles.header}/>
-          hey heyyy
       </header>
       <main className={styles.main}>
-      <div className="intro">
+      <div className="intro" style={{display: "flex"}}>
         <div className="egg image">
-        <img src="/pics/egg.jpg" alt="a smiling egg plushie" placeholder="blur" width={100}/>
-        </div>        
+        <img src="/pics/egg.jpg" alt="a smiling egg plushie" placeholder="blur" width={200}/>
+        </div>
+        <div className="intro text">     
         <h1>Hi, I'm Ezgi.</h1>
         <h2>What are you here to do?</h2>
         <div style={{ display: "flex" }}>
@@ -47,10 +47,11 @@ export default function Home() {
             }
           ></MakePaper>
       </div>
+      </div> 
     </div>
       </main>
       <footer className={styles.footer}>
-        feet
+        hey! this website is in progress! hopefully it will be super cool when it's done!
       </footer>
     </div>
   )
@@ -64,14 +65,19 @@ function Greeting(props) {
 
 function MakePaper(props) {
   return (
+    <div>
     <Box
-      sx={{
+      style={
+        {
         display: "flex",
-      }}
-    >
-      <Paper rounded="true" elevation={2} className={styles.card}>
+      }}>
+      <Paper rounded="true" elevation={2} className={styles.card} >
         <div>{props.contents}</div>
       </Paper>
     </Box>
+    <div style= {{
+      display: "block",
+      paddingBottom: '100%',}}/>
+    </div>
   );
 }
