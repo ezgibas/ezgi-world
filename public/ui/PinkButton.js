222f3e574caf6a2463463700302c44e4311a623e
Ezgi Bas <basezgi@gmail.com>
2023-07-02 21:36:09 -0400

    WE HAVE A BUTTON

--- public/ui/PinkButton.js
+++ public/ui/PinkButton.js
@@ -21,13 +21,14 @@ export default function PinkButton(props) {
                 </svg>
             </div>
             <div class={styles.hide}>
-                <a class={styles.buttonLink}></a>
-                <svg xmlns="http://www.w3.org/2000/svg" width={width} viewBox="0 -0.5 28 15" shape-rendering="crispEdges">
-                    <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
-                    <path stroke="#f2b6c3" d="M3 0h21M2 1h23M1 2h25M0 3h27M0 4h27M0 5h27M0 6h27M0 7h27M0 8h27M0 9h27M0 10h27M1 11h25M2 12h23M3 13h21" />
-                    <path stroke="#000000" d="M27 4h1M27 5h1M27 6h1M27 7h1M27 8h1M27 9h1M27 10h1M26 11h2M25 12h2M24 13h2M4 14h21" />
-                    <text x="1.5" y={yStr} font-size={fontSizeStr}>{text}</text>
-                </svg>
+                <a class={styles.buttonLink} href="https://google.com">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={width} viewBox="0 -0.5 28 15" shape-rendering="crispEdges">
+                        <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+                        <path stroke="#f2b6c3" d="M3 0h21M2 1h23M1 2h25M0 3h27M0 4h27M0 5h27M0 6h27M0 7h27M0 8h27M0 9h27M0 10h27M1 11h25M2 12h23M3 13h21" />
+                        <path stroke="#000000" d="M27 4h1M27 5h1M27 6h1M27 7h1M27 8h1M27 9h1M27 10h1M26 11h2M25 12h2M24 13h2M4 14h21" />
+                        <text x="1.5" y={yStr} font-size={fontSizeStr}>{text}</text>
+                    </svg>
+                </a>
             </div>
         </div>
 
